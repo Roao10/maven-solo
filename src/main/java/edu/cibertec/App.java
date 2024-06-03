24122ee5e450c5d5071c86cc1ebe70fa62e5e802
Roao10 <roalva17caro@gmail.com>
2024-06-03 11:58:04 -0500

    initial scaffold

---
+++ src/main/java/edu/cibertec/App.java
@@ -0,0 +1,11 @@
+package edu.cibertec;
+
+import org.apache.commons.collections4.*;
+
+public class App implements Unmodifiable
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
